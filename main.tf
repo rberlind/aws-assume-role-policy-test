6a0e0e0eea181ed891e27db7e5d7f89737a20499
Roger Berlind <rberlind@optonline.net>
2018-04-19 15:12:40 -0400

    Put region in right place

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 provider "aws" {
+  region     = "us-east-1"
   assume_role {
     role_arn     = "arn:aws:iam::128997349609:role/terraform-role"
     session_name = "SESSION_NAME"
@@ -7,7 +8,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  region     = "us-east-1"
   ami           = "ami-2e1ef954"
   instance_type = "t2.micro"
 
