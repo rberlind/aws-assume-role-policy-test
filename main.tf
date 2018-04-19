67eee59e44bbecc469ea58546321df5d797590d0
Roger Berlind <rberlind@optonline.net>
2018-04-19 15:39:52 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
+variable "role" {
+  description = "IAM Role to assume"
+}
+
 provider "aws" {
   region     = "us-east-1"
   assume_role {
-    role_arn     = "arn:aws:iam::128997349609:role/terraform-role"
+    #role_arn     = "arn:aws:iam::128997349609:role/terraform-role"
+    role_arn = "${var.role}"
     session_name = "SESSION_NAME"
     external_id  = "EXTERNAL_ID"
   }
